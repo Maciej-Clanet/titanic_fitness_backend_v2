e5d4f0f3ce9f4fd8b8db74abbb9926b6413b51b4
Maciej-Clanet <43215153+Maciej-Clanet@users.noreply.github.com>
2025-02-05 22:51:14 +0000

    adding workouts

--- models/workout_models.py
+++ models/workout_models.py
@@ -1,25 +1,38 @@
 from pydantic import BaseModel
 from typing import List, Dict
 
+
 class AddWorkout(BaseModel):
-    exercise_name : str
-    date : str
-    reps : int
+    exercise_name: str
+    date: str
+    reps: int
     weight: float
-    user_email : str
+    user_email: str
 
 
+# Data that will be send to the endpoint
 class GetDay(BaseModel):
     user_email: str
     date: str
+
 
 class ExerciseEntry(BaseModel):
     reps: int
     weight: float
 
+
+# Workout day contains a dictionary of exercises each having a list of exercise entries for logging
 class WorkoutDay(BaseModel):
-    exercises: Dict[str, List[ExerciseEntry] ] 
+    exercises: Dict[str, List[ExerciseEntry]]
 
 
 class Workouts(BaseModel):
     workouts: Dict[str, WorkoutDay]
+
+
+class AddExercise(BaseModel):
+    user_email: str
+    date: str
+    exercise_name: str
+    reps: int
+    weight: float
